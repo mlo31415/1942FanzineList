6b7092268874d3b6f8489aa2f755b56560c72eb3
Mark Olson <mlo@baskerville.org>
2018-02-08 11:05:01 -0500

    Cleanup and reorganization for clarity

--- 1942 Fanzine List.py
+++ 1942 Fanzine List.py
@@ -1,24 +1,7 @@
 from bs4 import BeautifulSoup
-from bs4 import NavigableString
 import requests
-import os
 import collections
-
-# Find text bracketed by <b>...</b>
-# Return the contents of the first pair of brackets found and the remainder of the input string
-def FindBracketedText(s, b):
-    strlower=s.lower()
-    l1=strlower.find("<"+b.lower())
-    if l1 == -1:
-        return "", ""
-    l1=strlower.find(">", l1)
-    if l1 == -1:
-        print("***Error: no terminating '>' found in "+strlower+"'")
-        return "", ""
-    l2=strlower.find("</"+b.lower()+">", l1+1)
-    if l2 == -1:
-        return "", ""
-    return s[l1+1:l2], s[l2+3+len(b):]
+import Helpers
 
 # Download the fanac.org webpage which lists all of the 1942 fanzine issues currently on the site
 h=requests.get("http://www.fanac.org/fanzines/Retro_Hugos.html")
@@ -40,19 +23,6 @@ for tag in table:
         continue
     line=tag.contents
 
-    #-------------------------------------
-    # Inline definition of a function to pull and hfref and accompanying text from a Tag
-    # The structure is "<a href='URL'>LINKTEXT</a>
-    # We want to extract the URL and LINKTEXT
-    def GetHrefAndTextFromTag(tag):
-        try:
-            href=tag.contents[0].attrs.get("href", None)
-        except:
-            href=tag.attrs.get("href")
-
-        return (tag.contents[0].string, href)
-    # -------------------------------------
-
     # The line is a list of tags and strings. Ignore the strings
     for tag2 in line:
         if tag2.string != None:
@@ -61,29 +31,19 @@ for tag in table:
         # Now we have a single fanzine entry. It has the format <li><a...></li>. We want the <a...> part
         # This is the first member of the tag's contents list.
         a=tag2.contents[0]
-        hrefLinkText, hrefUrl=GetHrefAndTextFromTag(a)
+        hrefLinkText, hrefUrl=Helpers.GetHrefAndTextFromTag(a)
         listOf1942s[hrefLinkText.lower()]=(hrefLinkText, hrefUrl)
 
 # Now we have a dictionary containing the names and URLs of the 1942 fanzines.
 # The next step is to figure out what 1942 issues of each we have on the website
 # We do this by reading the fanzines/<name>/index.html file and then decoding the table in it.
 
-#-----------------------------------------
-# Inline defintion of function to generate the proper kind of path.  (This may change depending on the target location of the output.)
-def RelPathToURL(relPath):
-    if relPath == None:
-        return "<no path>"
-    if relPath[0] == ".":
-        return "http://www.fanac.org/"+os.path.normpath(os.path.join("fanzines", relPath)).replace("\\", "/")
-    return relPath
-#-----------------------------------------
-
 # Loop over the list of fanzines
 for (key, (title, relPath)) in listOf1942s.items():
 
     # The URL we get is relative to the fanzines directory which has the URL fanac.org/fanzines
     # We need to turn relPath into a URL
-    url=RelPathToURL(relPath)
+    url=Helpers.RelPathToURL(relPath)
     print(title, "", url)
 
     # Download the index.html which lists all of the issues of the specified currently on the site
@@ -93,15 +53,6 @@ for (key, (title, relPath)) in listOf1942s.items():
     b = s.body.contents
     # Because the structures of the pages are so random, we need to search the body for the table.
     # *So far* all of the tables have been headed by <table border="1" cellpadding="5">, so we look for that.
-
-    #-----------------------------------------
-    # Define inline a simple function to name tags for debugging purposes
-    def N(tag):
-        try:
-            return tag.__class__.__name__
-        except:
-            return "Something"
-    #-----------------------------------------
 
     # ----------------------------------------
     # Inline definition of a function to search recursively for the table containing the fanzines listing
@@ -139,39 +90,21 @@ for (key, (title, relPath)) in listOf1942s.items():
     # The first row is the column headers
     # Subsequent rows are fanzine issue rows
 
-    #----------------------------------------
-    # Inline definition of a function to compress newline elements from a list of Tags.
-    def RemoveNewlineRows(tags):
-        compressedTags = []
-        for row in tags:
-            if not isinstance(row, NavigableString):
-                compressedTags.append(row)
-        return compressedTags
-    #----------------------------------------
-
     # Some of the items showing up in val.contents will be strings containing newlines -- start by compressing them out.
-    val.contents=RemoveNewlineRows(val.contents)
+    val.contents=Helpers.RemoveNewlineRows(val.contents)
 
     # Ok. We have the table.  Make a list of the column headers. We need to compress the newlines out of this as well
-    tableHeader = RemoveNewlineRows(val.contents[0])
+    tableHeader = Helpers.RemoveNewlineRows(val.contents[0])
     columnHeaders = []
     for col in tableHeader:
         columnHeaders.append(col.string)
 
-    #---------------------------------------
-    # Inline definition of a function to find the index of a string in a list of strings
-    def FindIndexOfStringInList(list, str):
-        for i in range(0, len(list) - 1):
-            if list[i] == str:
-                return i
-    #---------------------------------------
-
     # Next, we select just the rows for 1942
     # Note that the dates aren't especially consistent, either, so we have to do some searching
     # What column contains the year?
-    yearCol=FindIndexOfStringInList(columnHeaders, "Year")
-    issueCol=FindIndexOfStringInList(columnHeaders, "Issue")
-    titleCol=FindIndexOfStringInList(columnHeaders, "Title")
+    yearCol=Helpers.FindIndexOfStringInList(columnHeaders, "Year")
+    issueCol=Helpers.FindIndexOfStringInList(columnHeaders, "Issue")
+    titleCol=Helpers.FindIndexOfStringInList(columnHeaders, "Title")
     if issueCol == None:
         issueCol=titleCol
 
@@ -188,13 +121,13 @@ for (key, (title, relPath)) in listOf1942s.items():
     # We have to treat the Title colum specially, since it contains the critical href we need.
     rows=[]
     for i in range(1, len(val)):
-        tableRow=RemoveNewlineRows(val.contents[i])
+        tableRow=Helpers.RemoveNewlineRows(val.contents[i])
         row=[]
         for j in range(0, len(tableRow)-1):
             if (j != issueCol):
                 row.append(tableRow[j].string)
             else:
-                row.append(GetHrefAndTextFromTag(tableRow[j]))
+                row.append(Helpers.GetHrefAndTextFromTag(tableRow[j]))
 
         rows.append(row)
 
@@ -213,8 +146,8 @@ JoesData=collections.namedtuple("JoesData", "Name Editor Stuff")
 # Each line follows a vague pattern:
 # <title> '(' <name of editor(s) ')' <a usually comma-separated list of issues> <crap, frequently in parenthesis>
 # Store the parsed information in a list of tuples
-f=open("1942 Fanzine List.txt")
-fanzines1942=[]
+f=open("1942 All Fanzines List.txt")
+allFanzines1942=[]
 for line in f:  # Each line is a fanzine
     if line[-1:] == "\n":   # Drop the trailing newline
         line=line[:-1]
@@ -232,10 +165,10 @@ for line in f:  # Each line is a fanzine
         print("*** Could find closing ')' in '"+ line + "'")
         continue
 
-    fanzines1942.append(JoesData(line[:loc1-1], line[loc1+1:loc2].title(), line[loc2+1:]))
+    allFanzines1942.append(JoesData(line[:loc1-1], line[loc1+1:loc2].title(), line[loc2+1:]))
 
 f.close()
-print("---fanzines1942 list created with "+str(len(fanzines1942))+" elements")
+print("---fanzines1942 list created with "+str(len(allFanzines1942))+" elements")
 
 
 print("--- Read Links1942.txt")
@@ -254,14 +187,14 @@ links1942ColNames=line.split(" ")
 ExternalLinksData=collections.namedtuple("ExternalLinksData", line)
 
 # Now read the rest of the data.
-links1942=[]
+externalLinks1942=[]
 for line in f:  # Each line after the first is a link to an external fanzine
 
     temp=line.split(";")
     t2=[]
     for t in temp:
         t2.append(t.strip())
-    links1942.append(ExternalLinksData(*tuple(t2))) # Turn the list into a named tuple.
+    externalLinks1942.append(ExternalLinksData(*tuple(t2))) # Turn the list into a named tuple.
 
 f.close()
 print("--- Completed reading Links1942.txt")
@@ -272,10 +205,10 @@ print("--- Completed reading Links1942.txt")
 #   3. We highlight those fanzines which are eligible for a 1942 Hugo
 
 # Define a named tuple to hold the expanded data I get by combining all the sources
-ExpandedData=collections.namedtuple("ExpandedData", "Name Editor Stuff isHugoEligible, Name2, URL")
+ExpandedData=collections.namedtuple("ExpandedData", "Name Editor Stuff isHugoEligible Name2 URL Issues")
 
-for i in range(0, len(fanzines1942)):
-    fanzine=fanzines1942[i]
+for i in range(0, len(allFanzines1942)):
+    fanzine=allFanzines1942[i]
 
     # First we take the fanzine name from Joe's 1942 Fanzine List.txt and match it to a 1942 fanzine on fanac.org
     jname=fanzine[0]
@@ -302,7 +235,7 @@ for i in range(0, len(fanzines1942)):
 
     # If that didn't work, see if we have a match in the list of external links
     if name == None:
-        for ex in links1942:
+        for ex in externalLinks1942:
             if jname.lower() == ex.Title.lower():
                 name, url = (ex.Title, ex.URL)
                 print("   Found (3): " + name + " --> " + url)
@@ -317,17 +250,43 @@ for i in range(0, len(fanzines1942)):
         print("   Not found (5): " + jname)
 
     # Update the 1942 fanzines list with the new information
-    fanzines1942[i]=ExpandedData(fanzine[0], fanzine[1], fanzine[2], isHugoEligible, name, RelPathToURL(url))
+    allFanzines1942[i]=ExpandedData(fanzine[0], fanzine[1], fanzine[2], isHugoEligible, name, Helpers.RelPathToURL(url), None)
 
-    # OK, now the problem is to decode the crap at the end to form a list of issue numbers...or something...
-    # We'll start by trying to recignize *just* the case where we have a comma-separated list of numbers and nothing else.
+# OK, now the problem is to decode the crap at the end to form a list of issue numbers...or something...
+# We'll start by trying to recognize *just* the case where we have a comma-separated list of numbers and nothing else.
+for i in range(0, len(allFanzines1942)):
+    fz=allFanzines1942[i]
+
+    if fz.Stuff == None:       # Skip empty stuff
+        continue
+    stuff="".join(fz.Stuff.split()) # Compress out whitespace
+    if len(stuff) == 0:     # Skip if it's allwhitespace
+        continue
+    spl=stuff.split(",")
+    if len(spl) == 0:       # Probably can't happen
+        continue
+
+    # OK, spl is now a list of one or more comma-separated items from Stuff
+    # See if they're all numbers
+    isInt=True
+    for s in spl:
+        try:
+            int(s)
+        except:
+            isInt=False
+            break
+
+    if not isInt:
+        print("Not all integers: "+str(spl))
+
+
 
 print("---Generate the HTML")
 f=open("1942.html", "w")
 f.write("<body>\n")
 f.write("<ul>\n")
 # Create the HTML file
-for fz in fanzines1942:
+for fz in allFanzines1942:
     print(fz)
 
     htm=None
@@ -368,10 +327,5 @@ for fz in fanzines1942:
 
 f.write("</ul></body>")
 f.close()
-
-# To make life a bit easier, just print out the crap:
-for fz in fanzines1942:
-    print(fz.Stuff)
-
 
 i=0
---
+++ Helpers.py
@@ -0,0 +1,69 @@
+import os
+from bs4 import NavigableString
+
+# Find text bracketed by <b>...</b>
+# Return the contents of the first pair of brackets found and the remainder of the input string
+def FindBracketedText(s, b):
+    strlower=s.lower()
+    l1=strlower.find("<"+b.lower())
+    if l1 == -1:
+        return "", ""
+    l1=strlower.find(">", l1)
+    if l1 == -1:
+        print("***Error: no terminating '>' found in "+strlower+"'")
+        return "", ""
+    l2=strlower.find("</"+b.lower()+">", l1+1)
+    if l2 == -1:
+        return "", ""
+    return s[l1+1:l2], s[l2+3+len(b):]
+
+#-------------------------------------
+# Inline definition of a function to pull and hfref and accompanying text from a Tag
+# The structure is "<a href='URL'>LINKTEXT</a>
+# We want to extract the URL and LINKTEXT
+def GetHrefAndTextFromTag(tag):
+    try:
+        href=tag.contents[0].attrs.get("href", None)
+    except:
+        href=tag.attrs.get("href")
+
+    return (tag.contents[0].string, href)
+# -------------------------------------
+
+#-----------------------------------------
+# Inline defintion of function to generate the proper kind of path.  (This may change depending on the target location of the output.)
+def RelPathToURL(relPath):
+    if relPath == None:
+        return "<no path>"
+    if relPath[0] == ".":
+        return "http://www.fanac.org/"+os.path.normpath(os.path.join("fanzines", relPath)).replace("\\", "/")
+    return relPath
+#-----------------------------------------
+
+
+#-----------------------------------------
+# Define inline a simple function to name tags for debugging purposes
+def N(tag):
+    try:
+        return tag.__class__.__name__
+    except:
+        return "Something"
+#-----------------------------------------
+
+#----------------------------------------
+# Inline definition of a function to compress newline elements from a list of Tags.
+def RemoveNewlineRows(tags):
+    compressedTags = []
+    for row in tags:
+        if not isinstance(row, NavigableString):
+            compressedTags.append(row)
+    return compressedTags
+#----------------------------------------
+
+#---------------------------------------
+# Inline definition of a function to find the index of a string in a list of strings
+def FindIndexOfStringInList(list, str):
+    for i in range(0, len(list) - 1):
+        if list[i] == str:
+            return i
+#---------------------------------------
